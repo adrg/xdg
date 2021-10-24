09084b86acca94adb6843b374597834fc0ee6c73
Adrian-George Bostan <adrg@epistack.com>
2021-10-24 14:05:31 +0300

    Add test case for internal ExpandHome on Windows

--- internal/util/paths_windows_test.go
+++ internal/util/paths_windows_test.go
@@ -4,6 +4,7 @@
 package util_test
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/adrg/xdg/internal/util"
@@ -17,4 +18,17 @@ func TestKnownFolderPath(t *testing.T) {
 	require.Equal(t, expected, util.KnownFolderPath(nil, []string{"ProgramData"}, nil))
 	require.Equal(t, expected, util.KnownFolderPath(nil, nil, []string{expected}))
 	require.Equal(t, "", util.KnownFolderPath(nil, nil, nil))
+}
+
+func TestExpandHome(t *testing.T) {
+	home := `C:\Users\test`
+
+	require.Equal(t, home, util.ExpandHome(`%USERPROFILE%`, home))
+	require.Equal(t, filepath.Join(home, "appname"), util.ExpandHome(`%USERPROFILE%\appname`, home))
+
+	require.Equal(t, "", util.ExpandHome("", home))
+	require.Equal(t, home, util.ExpandHome(home, ""))
+	require.Equal(t, "", util.ExpandHome("", ""))
+
+	require.Equal(t, home, util.ExpandHome(home, home))
 }
